ee0c24f495990bc434f3fe3472bb2273540001c8
Jakob Hilden <jakobhilden@gmail.com>
2016-10-26 17:45:40 +0200

    fixup! Add option to also download space's assets during export

--- test/download-asset-test.js
+++ test/download-asset-test.js
@@ -4,16 +4,33 @@ import fs from 'fs-extra'
 import path from 'path'
 var tmpDirectory = path.join(__dirname, 'tmp')
 
-test('Download asset', (t) => {
+test('Download asset successfully', (t) => {
   fs.mkdirsSync(tmpDirectory)
 
   downloadAsset(
     '//images.contentful.com/kq9lln4hyr8s/2MTd2wBirYikEYkIIc0YSw/7aa4c06f3054996e45bb3f13964cb254/rocka-nutrition.png',
     tmpDirectory
-  )
-  // file should have been downloaded
+  ).then((file) => {
+    t.equal(file, tmpDirectory + '/images.contentful.com/kq9lln4hyr8s/2MTd2wBirYikEYkIIc0YSw/7aa4c06f3054996e45bb3f13964cb254/rocka-nutrition.png')
+  })
+  // test that file has has been downloaded
+  // by checking if access throws no exception
   t.doesNotThrow(function () {
     fs.accessSync(tmpDirectory + '/images.contentful.com/kq9lln4hyr8s/2MTd2wBirYikEYkIIc0YSw/7aa4c06f3054996e45bb3f13964cb254/rocka-nutrition.png')
+  })
+
+  fs.removeSync(tmpDirectory)
+  t.end()
+})
+
+test('Download fails', (t) => {
+  fs.mkdirsSync(tmpDirectory)
+
+  downloadAsset(
+    '//images.contentful.com/does-not-exist.png',
+    tmpDirectory
+  ).catch((error) => {
+    t.equal(error, 'error response status: 404')
   })
 
   fs.removeSync(tmpDirectory)
