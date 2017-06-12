c04f406e479e31c8f8aa0f7635c04fdaffe086c0
Khaled Garbaya <khaled@contentful.com>
2017-06-12 13:17:30 +0200

    test(contentful-export): Fix cli tests

--- test/integration/export-cli-test.js
+++ test/integration/export-cli-test.js
@@ -2,9 +2,9 @@ import test from 'tape'
 import nixt from 'nixt'
 import { join } from 'path'
 import fs from 'fs'
-
+import rimraf from 'rimraf'
 const bin = join(__dirname, '../../', 'bin')
-
+const tmpFolder = join(__dirname, 'tmp')
 const app = () => {
   return nixt({ newlines: true }).cwd(bin).base('./contentful-export').clone()
 }
@@ -13,18 +13,11 @@ const spaceId = process.env.EXPORT_SPACE_ID
 const managementToken = process.env.MANAGEMENT_TOKEN
 let filePath = null
 
-test.onFinish(() => {
-  try {
-    fs.unlinkSync(join(bin, filePath))
-  } catch (e) {
-
-  }
-})
-
 test('It should export space properly when running as a cli', (t) => {
   t.plan(7)
+  fs.mkdirSync(tmpFolder)
   app()
-    .run(` --space-id ${spaceId} --management-token ${managementToken}`)
+    .run(` --space-id ${spaceId} --management-token ${managementToken} --export-dir ${tmpFolder}`)
     .stdout(/Exported entities/)
     .stdout(/Content Types/)
     .stdout(/Editor Interfaces/)
@@ -34,12 +27,9 @@ test('It should export space properly when running as a cli', (t) => {
     .stdout(/Webhooks/)
     .stdout(/Roles/)
     .end(() => {
-      const fileList = fs.readdirSync(bin)
-      const exportedFileIndex = fileList.findIndex((element) => {
-        return element.indexOf(`contentful-export-${spaceId}`) >= 0
-      })
-      filePath = fileList[exportedFileIndex]
-      const exportedFile = JSON.parse(fs.readFileSync(join(bin, filePath)))
+      const fileList = fs.readdirSync(tmpFolder)
+      filePath = fileList[0] // we only have one file
+      const exportedFile = JSON.parse(fs.readFileSync(join(tmpFolder, filePath)))
       t.equal(exportedFile.contentTypes.length, 3, '3 content types should be exported')
       t.equal(exportedFile.editorInterfaces.length, 3, '3 editor interfaces should be exported')
       t.equal(exportedFile.entries.length, 6, '6 entries should be exported')
@@ -47,6 +37,8 @@ test('It should export space properly when running as a cli', (t) => {
       t.equal(exportedFile.locales.length, 1, '1 locale should be exported')
       t.equal(exportedFile.webhooks.length, 0, '0 webhooks should be exported')
       t.equal(exportedFile.roles.length, 7, '7 roles should be exported')
-      t.end()
+      rimraf(tmpFolder, () => {
+        t.end()
+      })
     })
 })
