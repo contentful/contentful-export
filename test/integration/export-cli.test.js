0da10a83197332ff037934b51cf646453d0af313
Benedikt Rötsch <benedikt@contentful.com>
2018-03-05 17:09:42 +0100

    fixup! feat(environments): add support to export environments

--- test/integration/export-cli.test.js
+++ test/integration/export-cli.test.js
@@ -8,7 +8,7 @@ import rimraf from 'rimraf'
 jest.setTimeout(15000)
 
 const bin = join(__dirname, '../../', 'bin')
-const tmpFolder = join(__dirname, 'tmp')
+const tmpFolder = join(__dirname, 'tmp-cli')
 const app = () => {
   return nixt({ newlines: true }).cwd(bin).base('./contentful-export').clone()
 }
@@ -17,8 +17,15 @@ const spaceId = process.env.EXPORT_SPACE_ID
 const managementToken = process.env.MANAGEMENT_TOKEN
 let filePath = null
 
-test('It should export space properly when running as a cli', (done) => {
+beforeAll(() => {
   mkdirp.sync(tmpFolder)
+})
+
+afterAll(() => {
+  rimraf.sync(tmpFolder)
+})
+
+test('It should export space properly when running as a cli', (done) => {
   app()
     .run(` --space-id ${spaceId} --management-token ${managementToken} --export-dir ${tmpFolder}`)
     .code(0)
@@ -39,7 +46,7 @@ test('It should export space properly when running as a cli', (done) => {
       if (!fileList.length) {
         throw new Error('No file was exported. Did you set EXPORT_SPACE_ID and MANAGEMENT_TOKEN env variables?')
       }
-      filePath = fileList[0] // we only have one file
+      filePath = fileList.find((filename) => filename.match(/master/))
       const exportedFile = JSON.parse(fs.readFileSync(join(tmpFolder, filePath)))
       expect(exportedFile.contentTypes).toHaveLength(2)
       expect(exportedFile.editorInterfaces).toHaveLength(2)
@@ -48,6 +55,41 @@ test('It should export space properly when running as a cli', (done) => {
       expect(exportedFile.locales).toHaveLength(1)
       expect(exportedFile.webhooks).toHaveLength(0)
       expect(exportedFile.roles).toHaveLength(7)
+      done()
+    })
+})
+
+test('It should export environment properly when running as a cli', (done) => {
+  mkdirp.sync(tmpFolder)
+  app()
+    .run(` --space-id ${spaceId} --environment-id staging --management-token ${managementToken} --export-dir ${tmpFolder}`)
+    .code(0)
+    .stdout(/Exported entities/)
+    .stdout(/Content Types +│ 2/)
+    .stdout(/Editor Interfaces +│ 2/)
+    .stdout(/Entries +│ 4/)
+    .stdout(/Assets +│ 4/)
+    .stdout(/Locales +│ 1/)
+    .stdout(/Webhooks can only be exported from master environment/)
+    .stdout(/Roles can only be exported from master environment/)
+    .end((error) => {
+      if (error) {
+        console.error(error)
+        throw new Error('Should not throw error')
+      }
+      const fileList = fs.readdirSync(tmpFolder)
+      if (!fileList.length) {
+        throw new Error('No file was exported. Did you set EXPORT_SPACE_ID and MANAGEMENT_TOKEN env variables?')
+      }
+      filePath = fileList.find((filename) => filename.match(/staging/))
+      const exportedFile = JSON.parse(fs.readFileSync(join(tmpFolder, filePath)))
+      expect(exportedFile.contentTypes).toHaveLength(2)
+      expect(exportedFile.editorInterfaces).toHaveLength(2)
+      expect(exportedFile.entries).toHaveLength(4)
+      expect(exportedFile.assets).toHaveLength(4)
+      expect(exportedFile.locales).toHaveLength(1)
+      expect(exportedFile).not.toHaveProperty('webhooks')
+      expect(exportedFile).not.toHaveProperty('roles')
       rimraf(tmpFolder, () => {
         done()
       })
--- test/integration/export-lib.test.js
+++ test/integration/export-lib.test.js
@@ -1,12 +1,26 @@
+import { join } from 'path'
+
+import mkdirp from 'mkdirp'
+import rimraf from 'rimraf'
+
 import runContentfulExport from '../../dist/index'
 
 jest.setTimeout(15000)
 
+const tmpFolder = join(__dirname, 'tmp-lib')
 const spaceId = process.env.EXPORT_SPACE_ID
 const managementToken = process.env.MANAGEMENT_TOKEN
 
+beforeAll(() => {
+  mkdirp.sync(tmpFolder)
+})
+
+afterAll(() => {
+  rimraf.sync(tmpFolder)
+})
+
 test('It should export space when used as a library', () => {
-  return runContentfulExport({spaceId, managementToken, saveFile: false})
+  return runContentfulExport({spaceId, managementToken, saveFile: false, exportDir: tmpFolder})
     .catch((multierror) => {
       const errors = multierror.errors.filter((error) => error.hasOwnProperty('error'))
       expect(errors).toHaveLength(0)
@@ -20,5 +34,23 @@ test('It should export space when used as a library', () => {
       expect(content.locales).toHaveLength(1)
       expect(content.webhooks).toHaveLength(0)
       expect(content.roles).toHaveLength(7)
+    })
+})
+
+test('It should export environment when used as a library', () => {
+  return runContentfulExport({spaceId, environmentId: 'staging', managementToken, saveFile: false, exportDir: tmpFolder})
+    .catch((multierror) => {
+      const errors = multierror.errors.filter((error) => error.hasOwnProperty('error'))
+      expect(errors).toHaveLength(0)
+    })
+    .then((content) => {
+      expect(content).toBeTruthy()
+      expect(content.contentTypes).toHaveLength(2)
+      expect(content.editorInterfaces).toHaveLength(2)
+      expect(content.entries).toHaveLength(4)
+      expect(content.assets).toHaveLength(4)
+      expect(content.locales).toHaveLength(1)
+      expect(content).not.toHaveProperty('webhooks')
+      expect(content).not.toHaveProperty('roles')
     })
 })
