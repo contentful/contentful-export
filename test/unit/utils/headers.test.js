d1a290876739643b8e9a28144fd26177ed328dee
Ahmed T. Ali <ah.tajelsir@gmail.com>
2021-04-13 16:28:10 +0200

    test: add tests

---
+++ test/unit/utils/headers.test.js
@@ -0,0 +1,35 @@
+const { getHeadersConfig } = require('../../../lib/utils/headers')
+
+test('getHeadersConfig returns empty object when value is undefined', () => {
+  expect(getHeadersConfig(undefined)).toEqual({})
+})
+
+test('getHeadersConfig accepts single or multiple values', () => {
+  expect(getHeadersConfig('Accept: Any')).toEqual({ Accept: 'Any' })
+  expect(getHeadersConfig(['Accept: Any', 'X-Version: 1'])).toEqual({
+    Accept: 'Any',
+    'X-Version': '1'
+  })
+})
+
+test('getHeadersConfig ignores invalid headers', () => {
+  expect(
+    getHeadersConfig(['Accept: Any', 'X-Version: 1', 'invalid'])
+  ).toEqual({
+    Accept: 'Any',
+    'X-Version': '1'
+  })
+})
+
+test('getHeadersConfig trims spacing around keys & values', () => {
+  expect(
+    getHeadersConfig([
+      '  Accept:   Any   ',
+      '   X-Version   :1 ',
+      'invalid'
+    ])
+  ).toEqual({
+    Accept: 'Any',
+    'X-Version': '1'
+  })
+})
