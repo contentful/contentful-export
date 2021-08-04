628f4415fd1744bc093b4db73e048e4fc50eeb97
Mehdi Hasan Khan <mehdi.khan@contentful.com>
2021-08-04 13:34:34 +0200

    feat: add unit test for embargoed asset

--- test/unit/tasks/download-assets.test.js
+++ test/unit/tasks/download-assets.test.js
@@ -9,8 +9,17 @@ import downloadAssets from '../../../lib/tasks/download-assets'
 const tmpDirectory = resolve(tmpdir(), 'contentful-import-test')
 
 const BASE_PATH = '//images.contentful.com'
+const BASE_PATH_SECURE = '//images.secure.contentful.com'
 const EXISTING_ASSET_URL = '/kq9lln4hyr8s/2MTd2wBirYikEYkIIc0YSw/7aa4c06f3054996e45bb3f13964cb254/rocka-nutrition.png'
+const EMBARGOED_ASSET_URL = '/kq9lln4hyr8s/2MTd2wBirYikEYkIIc0YSw/7aa4c06f3054996e45bb3f13964cb254/space-dog.png'
 const NON_EXISTING_URL = '/does-not-exist.png'
+
+const API_HOST = 'api.contentful.com'
+const SPACE_ID = 'kq9lln4hyr8s'
+const ACCESS_TOKEN = 'abc'
+const ENVIRONMENT_ID = 'master'
+const POLICY = 'eyJhbG.eyJMDIyfQ.SflKx5c'
+const SECRET = 's3cr3t'
 
 let taskProxy
 let output
@@ -24,8 +33,24 @@ nock(`https:${BASE_PATH}`)
   .get(NON_EXISTING_URL)
   .reply(404)
 
-function getAssets ({ existing = 0, nonExisting = 0, missingUrl = 0 } = {}) {
+// Mock downloading assets using signed URLs
+nock(`https:${BASE_PATH_SECURE}`)
+  .get(EMBARGOED_ASSET_URL)
+  .query({ policy: POLICY, token: /.+/i })
+  .times(2)
+  .reply(200)
+
+// Mock asset-key creation for embargoed assets
+nock(`https://${API_HOST}`)
+  .post(`/spaces/${SPACE_ID}/environments/${ENVIRONMENT_ID}/asset_keys`, {
+    expiresAt: /.+/i
+  })
+  .times(2)
+  .reply(200, { policy:POLICY, secret: SECRET })
+
+function getAssets ({ existing = 0, nonExisting = 0, missingUrl = 0, embargoed = 0 } = {}) {
   const existingUrl = `${BASE_PATH}${EXISTING_ASSET_URL}`
+  const embargoedUrl = `${BASE_PATH_SECURE}${EMBARGOED_ASSET_URL}`
   const nonExistingUrl = `${BASE_PATH}${NON_EXISTING_URL}`
   const assets = []
   for (let i = 0; i < nonExisting; i++) {
@@ -60,6 +85,25 @@ function getAssets ({ existing = 0, nonExisting = 0, missingUrl = 0 } = {}) {
           },
           'de-DE': {
             url: existingUrl,
+            upload: '//file-stack-url-do-not-use-me.png'
+          }
+        }
+      }
+    })
+  }
+  for (let i = 0; i < embargoed; i++) {
+    assets.push({
+      sys: {
+        id: `embargoed asset ${i}`
+      },
+      fields: {
+        file: {
+          'en-US': {
+            url: embargoedUrl,
+            upload: '//file-stack-url-do-not-use-me.png'
+          },
+          'de-DE': {
+            url: embargoedUrl,
             upload: '//file-stack-url-do-not-use-me.png'
           }
         }
@@ -104,6 +148,13 @@ beforeAll(() => {
 
 afterAll(() => {
   fs.removeSync(tmpDirectory)
+
+  if (!nock.isDone()) {
+    throw new Error(`pending mocks: ${nock.pendingMocks().join(", ")}`)
+  }
+
+  nock.cleanAll()
+  nock.restore()
 })
 
 test('Downloads assets and properly counts failed attempts', () => {
@@ -132,6 +183,33 @@ test('Downloads assets and properly counts failed attempts', () => {
         errorCount: 2
       })
       expect(output.mock.calls).toHaveLength(5)
+    })
+})
+
+test('Downloads embargoed assets', () => {
+  const task = downloadAssets({
+    exportDir: tmpDirectory,
+    host: API_HOST,
+    accessToken: ACCESS_TOKEN,
+    spaceId: SPACE_ID,
+    environmentId:ENVIRONMENT_ID
+  })
+  const ctx = {
+    data: {
+      assets: [
+        ...getAssets({ embargoed: 1 })
+      ]
+    }
+  }
+
+  return task(ctx, taskProxy)
+    .then(() => {
+      expect(ctx.assetDownloads).toEqual({
+        successCount: 2,
+        warningCount: 0,
+        errorCount: 0
+      })
+      expect(output.mock.calls).toHaveLength(2)
     })
 })
 
